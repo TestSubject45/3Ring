16bc43e19558429dd9082e8a78a8ec993cbf4b08
Blake <blake@blakegambel.com>
2019-04-08 17:05:53 -0600

    Began adding button layout, no functionality yet
    Messed with BoxSizer and GridSizer, so working on getting stuff layed out.

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from funcs import *
 class mainFrame(wx.Frame):
 
     def __init__(self, parent, title):
-        super(mainFrame, self).__init__(parent,title=title,size=(500,300))
+        super(mainFrame, self).__init__(parent,title=title,size=(1500,800))
 
         self.InitUI()
 
@@ -20,13 +20,58 @@ class mainFrame(wx.Frame):
 
     def InitUI(self):
 
+        #Create Panel and sizers
         panel = wx.Panel(self,wx.ID_ANY)
-        button = wx.Button(panel,wx.ID_ANY,'TEST',(50,150))
+        MainSizer = wx.BoxSizer(orient=wx.HORIZONTAL)
+
+        LeftSizer = wx.BoxSizer(orient=wx.VERTICAL)
+        homeButtonSizer = wx.GridSizer(4,4,10,20)
+
         
-        self.textbox = wx.TextCtrl(panel,size=(200,100),style=wx.TE_MULTILINE)
+        #Initialize Buttons
+        SubmitButton = wx.Button(panel,wx.ID_ANY,'Submit',(10,50))
 
-        button.Bind(wx.EVT_BUTTON,self.onButton)
+        button1 = wx.Button(panel,wx.ID_ANY,'TEMP1',(30,50))
+        button2 = wx.Button(panel,wx.ID_ANY,'TEMP2',(30,50))
+        button3 = wx.Button(panel,wx.ID_ANY,'TEMP3',(30,50))
+        button4 = wx.Button(panel,wx.ID_ANY,'TEMP4',(30,50))
+        button5 = wx.Button(panel,wx.ID_ANY,'TEMP5',(30,50))
+        button6 = wx.Button(panel,wx.ID_ANY,'TEMP6',(30,50))
+        button7 = wx.Button(panel,wx.ID_ANY,'TEMP7',(30,50))
+        button8 = wx.Button(panel,wx.ID_ANY,'TEMP8',(30,50))
 
+
+
+        #initialize textbox and bind button to it
+        self.textbox = wx.TextCtrl(panel,size=(600,200),style=wx.TE_MULTILINE)
+        SubmitButton.Bind(wx.EVT_BUTTON,self.onButton)
+
+        #Add objects to sizers
+        homeButtonSizer.Add(button1,1,wx.FIXED_MINSIZE,10)
+        homeButtonSizer.Add(button2,1,wx.FIXED_MINSIZE,10)
+        homeButtonSizer.Add(button3,1,wx.FIXED_MINSIZE,10)
+        homeButtonSizer.Add(button4,1,wx.FIXED_MINSIZE,10)
+        homeButtonSizer.Add(button5,1,wx.FIXED_MINSIZE,10)
+        homeButtonSizer.Add(button6,1,wx.FIXED_MINSIZE,10)
+        homeButtonSizer.Add(button7,1,wx.FIXED_MINSIZE,10)
+        homeButtonSizer.Add(button8,1,wx.FIXED_MINSIZE,10)
+
+
+        LeftSizer.AddSpacer(50)
+        LeftSizer.Add(homeButtonSizer,1,wx.EXPAND,10)
+        LeftSizer.Add(self.textbox,0,wx.FIXED_MINSIZE,10)
+        LeftSizer.Add(SubmitButton,0,wx.ALL|wx.FIXED_MINSIZE,10)
+
+        MainSizer.AddSpacer(50)
+        MainSizer.Add(LeftSizer,1,wx.EXPAND,10)
+        MainSizer.AddSpacer(800)
+
+
+
+
+
+        panel.SetSizer(MainSizer)
+        #Call other function to init menu
         self.InitMenu()
 
     def onButton(self,event):
