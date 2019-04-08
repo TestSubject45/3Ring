957a5de505775b8a776124f1ee82c1b017e14d69
Blake <blake@blakegambel.com>
2019-04-08 16:20:30 -0600

    got text input working
    Finally got text input working, so I can now start building out the rest of the gui

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from funcs import *
 class mainFrame(wx.Frame):
 
     def __init__(self, parent, title):
-        super(mainFrame, self).__init__(parent,title=title,size=(1500,800))
+        super(mainFrame, self).__init__(parent,title=title,size=(500,300))
 
         self.InitUI()
 
@@ -21,15 +21,19 @@ class mainFrame(wx.Frame):
     def InitUI(self):
 
         panel = wx.Panel(self,wx.ID_ANY)
-        button = wx.Button(panel,wx.ID_ANY,'TEST',(10,10))
+        button = wx.Button(panel,wx.ID_ANY,'TEST',(50,150))
+        
+        self.textbox = wx.TextCtrl(panel,size=(200,100),style=wx.TE_MULTILINE)
+
         button.Bind(wx.EVT_BUTTON,self.onButton)
 
         self.InitMenu()
 
-    def onButton(self,e):
-        print("Button Clicked")
+    def onButton(self,event):
+        inpt = self.textbox.GetValue()
+        print(inpt)
         
-    def OnQuit(self,e):
+    def OnQuit(self,event):
         print("EX")
         self.Close()
         exit()
